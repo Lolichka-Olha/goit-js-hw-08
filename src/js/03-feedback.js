116e9fa6ce2ddb1095708ac13740af49ca2c948f
Olha Truba <o.ljolik@gmail.com>
2022-05-30 04:11:04 +0300

    adds task 3

--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -8,41 +8,37 @@ const refs = {
 
 const STORAGE_KEY = 'feedback-form-state';
 
-refs.form.addEventListener('submit', onFormSubmit);
 refs.form.addEventListener('input', throttle(onFormInput, 500));
+refs.form.addEventListener('submit', onFormSubmit);
+
+populateTextarea();
 
 function onFormInput(event) {
   event.preventDefault();
 
-  const formData = {
+  const value = {
     email: refs.email.value,
     message: refs.message.value,
   };
-  localStorage.setItem(STORAGE_KEY, JSON.stringify(formData));
+  localStorage.setItem(STORAGE_KEY, JSON.stringify(value));
 }
 
 function onFormSubmit(event) {
   event.preventDefault();
 
-  //   const formData = {
-  //     email: refs.email.value,
-  //     message: refs.message.value,
-  //   };
   console.log(localStorage.getItem(STORAGE_KEY));
 
+  localStorage.removeItem(STORAGE_KEY);
   refs.form.reset();
   //   refs.email.value = '';
   //   refs.message.value = '';
-  localStorage.removeItem(STORAGE_KEY);
 }
 
-populateTextarea();
-
 function populateTextarea() {
-  const savedMessage = JSON.parse(localStorage.getItem(STORAGE_KEY));
+  const savedMessage = JSON.parse(localStorage.getItem(STORAGE_KEY)) || {};
 
-  if (savedMessage) {
-    refs.email.value = savedMessage.email;
-    refs.message.value = savedMessage.message;
-  }
+  refs.email.value = savedMessage.email || '';
+  refs.message.value = savedMessage.message || '';
+
+  //   localStorage.setItem(STORAGE_KEY, JSON.stringify(savedMessage));
 }
